dbbb4007d0d8ddff7283c436cbf99553bb96244c
yonexyonex <yonexyonex@icloud.com>
2015-06-05 02:09:21 +0900

    update mutiple quote filter

---
+++ spec/slack_markdown/filters/multiple_quote_filter_spec.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe SlackMarkdown::Filters::MultipleQuoteFilter do
+  subject do
+    filter = SlackMarkdown::Filters::MultipleQuoteFilter.new(text)
+    filter.call.to_s
+  end
+
+  context '>>> hoge' do
+    let(:text) { '&gt;&gt;&gt; hoge' }
+    it { should eq "<blockquote>hoge</blockquote>" }
+  end
+
+  context 'multiline' do
+    let(:text) { "&gt;&gt;&gt;\nhoge\nfuga" }
+    it { should eq "<blockquote>hoge\nfuga</blockquote>" }
+  end
+end
